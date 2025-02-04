7e3a7f3d380ef189721fc600169c7eedacb231ca
D.VAMSI TEJ CHOWDARY <d.vamsitej333@gmail.com>
2025-02-04 18:59:02 -0500

    modified_session_routing

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,12 +4,12 @@ import { redirect } from "next/navigation";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Register() {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  if (session) {
-    // If already logged in, redirect to the dashboard
-    redirect("/dashboard");
-  }
+  // if (session) {
+  //   // If already logged in, redirect to the dashboard
+  //   redirect("/dashboard");
+  // }
 
   return <LoginForm />;
 }
--- middleware.js
+++ middleware.js
@@ -1,3 +1,24 @@
-export { default } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-export const config = { matcher: ["/dashboard"] };
+export async function middleware(req) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const { pathname } = req.nextUrl;
+
+
+  if (token && (pathname === "/login" || pathname === "/register")) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  // Redirect logged-out users away from /dashboard to /login
+  if (!token && pathname === "/dashboard") {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+
+export const config = {
+  matcher: ["/login", "/register", "/dashboard"], // Monitor these routes
+};
